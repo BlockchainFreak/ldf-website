bf08bb5e98f7cf6925ba4530f674e476f5ed5d18
Umer <25100029@lums.edu.pk>
2023-12-29 19:03:09 +0500

    Header: hrefs added to tabs + dropdown menu refractor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,57 @@
 import React from 'react';
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from '@/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
+
+const tabs = [
+    { name: 'Info', href: '#' },
+    { name: 'Talks', href: '#talks' },
+    { name: 'Speakers', href: '#speakers' },
+    { name: 'Subevents', href: '#events' },
+    { name: 'Sponsors', href: '#sponsors' },
+    { name: 'Contact Us', href: '#contact' },
+    { name: 'About Us', href: '#about' },
+]
 
 const Navigation = () => {
-  return (
-    <div className="flex flex-row lg:space-x-8 py-3 px-6 lg:px-32 justify-between items-center bg-black text-white">
-      <img src="/images/frame.svg" className="" />
+    return (
+        <div className="flex flex-row lg:space-x-8 py-3 px-6 lg:px-32 justify-between items-center bg-black text-white">
+            <img src="/images/frame.svg" className="" />
 
-      {/* Dropdown for small screens */}
-      <div className="lg:hidden">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <button>
-                <img src="/burger-menu.svg" className="w-8 h-8" />
-            </button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuItem>Info</DropdownMenuItem>
-            <DropdownMenuItem>Talks</DropdownMenuItem>
-            <DropdownMenuItem>Speakers</DropdownMenuItem>
-            <DropdownMenuItem>Subevents</DropdownMenuItem>
-            <DropdownMenuItem>Sponsors</DropdownMenuItem>
-            <DropdownMenuItem>Contact Us</DropdownMenuItem>
-            <DropdownMenuItem>About Us</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+            {/* Dropdown for small screens */}
+            <div className="lg:hidden">
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <button>
+                            <img src="/burger-menu.svg" className="w-8 h-8" />
+                        </button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56 border-zinc-600 bg-zinc-800 text-white">
+                        {
+                            tabs.map((tab) => (
+                                <DropdownMenuItem key={tab.name}>
+                                    <a>{tab.name}</a>
+                                </DropdownMenuItem>
+                            ))
+                        }
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>
+                            Register Now
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
 
-      {/* Tabs for larger screens */}
-      <div className="hidden lg:flex lg:flex-row lg:space-x-8 lg:justify-center lg:px-12">
-        <div>Info</div>
-        <div>Talks</div>
-        <div>Speakers</div>
-        <div>Subevents</div>
-        <div>Sponsors</div>
-        <div>Contact Us</div>
-        <div>About Us</div>
-      </div>
+            {/* Tabs for larger screens */}
+            <div className="hidden lg:flex lg:flex-row lg:space-x-8 lg:justify-center lg:px-12">
+                {
+                    tabs.map((tab) => (
+                        <a key={tab.name} href={tab.href}>{tab.name}</a>
+                    ))
+                }
+            </div>
 
-      <div className="hidden lg:flex lg:flex-row lg:space-x-8">Register Now</div>
-    </div>
-  );
+            <div className="hidden lg:flex lg:flex-row lg:space-x-8">Register Now</div>
+        </div>
+    );
 };
 
 export default Navigation;
