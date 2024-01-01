7e1de42f6969cd51d9a28a584768098c9f8c87b5
Umar Zubair <umerzubair960@gmail.com>
2024-01-02 01:21:33 +0500

    Added some images,made sponsors section,made contact us and speakers page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // plugins: [require("@tailwindcss/forms")],
   darkMode: ["class"],
   content: [""],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
