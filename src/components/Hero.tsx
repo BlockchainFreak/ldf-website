a6e48691a7aba39a791b301c1cfb1e7005d971cd
Umer <25100029@lums.edu.pk>
2024-01-03 17:24:12 +0500

    Update HeroSection component styles + Hero Logo to Gradient LDF Logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,14 @@
 export default function HeroSection() {
     return (
-        <section className="flex justify-center">
-            <div className="flex flex-col gap-12 w-80 lg:w-[44rem]">
+        <section className="flex justify-center mt-4">
+            <div className="flex flex-col gap-8 w-80 lg:w-[32rem]">
                 <div className="relative">
                     <div className="absolute">
-                        <img src="/purple-stars.svg" className="relative bottom-10 right-10 w-16 lg:bottom-16 lg:right-12 lg:w-24"/>
+                        <img src="/purple-stars.svg" className="relative bottom-10 lg:bottom-8 right-10 lg:right-12 w-16 lg:w-24"/>
                     </div>
-                    <img src="/ldf-logo.svg" className="w-full"/>
+                    <img src="/hero-logo.png" className="w-full"/>
                 </div>
-                <div className="w-full flex flex-col gap-16 items-center">
+                <div className="w-full flex flex-col gap-8 items-center">
                     <div className="flex justify-between text-white w-80 font-SpaceGrotesk">
                         <span>JAN 19, 20, 21</span>
                         <span className="text-gray-400">|</span>
