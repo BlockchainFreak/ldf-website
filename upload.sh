f6e72121b33b7f3cc436bd75dc82857402980ca5
Umer <25100029@lums.edu.pk>
2024-01-12 23:48:20 +0500

    Add line to ignore certificate verification in upload.sh

--- upload.sh
+++ upload.sh
@@ -6,6 +6,7 @@ FTP_USER="emperien_ldf_ftp_user"
 
 # Log in to FTP server and upload contents of /dist recursively
 lftp -e "
+set ssl:verify-certificate no; # Add this line to ignore certificate verification
 open $FTP_HOST
 user $FTP_USER $FTP_PASS
 mirror --reverse --verbose dist /astro_build
